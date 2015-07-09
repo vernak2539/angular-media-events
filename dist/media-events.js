aa62ceb27ccde997630fa410028d5b8060513d83
Alex Vernacchia <alvernacchia@gmail.com>
2015-07-09 21:58:30 +0100

    Release 0.1.0

--- dist/media-events.js
+++ dist/media-events.js
@@ -9,17 +9,29 @@
 
   angular
     .module('media-events')
-    .directive('loadedMetadata', function() {
-      return {
-        restrict: 'E',
-        scope: true,
-        link: function(scope, element, attrs) {
-          element.bind('loadedmetadata', function() {
-            scope.element = this;
+    .directive('loadedMetadata', [
+      function() {
+        return {
+          restrict: 'A',
+          scope: true,
+          link: function(scope, element, attrs) {
+            element.on('loadedmetadata', function(event) {
+              var locals = {
+                $event: event,
+                attrs: {
+                  width: this.videoWidth,
+                  height: this.videoHeight
+                }
+              };
 
-            scope.$eval(attrs.loadedMetadata);
-          });
-        }
-      };
-    });
+              // Need to figure out what an error looks like. PR would be great
+              // https://github.com/vernak2539/angular-media-events/issues/1
+
+              scope.$eval(attrs.loadedMetadata, locals);
+            });
+          }
+        };
+      }
+    ]);
+
 })();
