0bb2dda4023f50a0ffec4e3b5a925221f2f6e6bb
Alex Vernacchia <alvernacchia@gmail.com>
2015-07-11 20:48:58 +0100

    removing forgotten commented code

--- tests/specs/progress.js
+++ tests/specs/progress.js
@@ -1,28 +1,3 @@
-// (function() {
-//   'use strict';
-//
-//   angular
-//     .module('media-events')
-//     .directive('progress', [
-//       function() {
-//         return {
-//           restrict: 'A',
-//           scope: true,
-//           link: function(scope, element, attrs) {
-//             element.on('progress', function(event) {
-//               var locals = {
-//                 $event: event
-//               };
-//
-//               scope.$eval(attrs.progress, locals);
-//             })
-//           }
-//         }
-//       }
-//     ])
-// })();
-
-
 (function() {
   'use strict';
 
