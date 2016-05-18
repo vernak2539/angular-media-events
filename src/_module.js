6416a001dbec68d9d64b8bfa19da5ec65a0f2999
Alex Vernacchia <alvernacchia@gmail.com>
2016-05-18 21:56:31 +0100

    export module name

--- src/_module.js
+++ src/_module.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const MODULE_NAME = 'media-events';
+
 import EvalService  from './eval-service';
 import LoadedMetadata from './loadedmetadata';
 import Progress from './progress';
@@ -7,9 +9,11 @@ import Play from './play';
 import Pause from './pause';
 
 angular
-    .module('media-events', [])
+    .module(MODULE_NAME, [])
     .service(EvalService.name, EvalService.main)
     .directive(LoadedMetadata.name, LoadedMetadata.main)
     .directive(Progress.name, Progress.main)
     .directive(Play.name, Play.main)
     .directive(Pause.name, Pause.main);
+
+module.exports = MODULE_NAME;
